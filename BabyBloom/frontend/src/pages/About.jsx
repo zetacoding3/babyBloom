0992ac920edc53eebdf2a0bf6ff38f01d6111373
zetacoding3 <zetacoding.academics@gmail.com>
2025-05-10 09:15:01 +0530

    initial commit

---
+++ BabyBloom/frontend/src/pages/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <div className="container-fluid g-0 px-3 py-5">
+                <img src="/img/about.avif" alt="" className="img-fluid col-md-4 float-start mx-4" />
+                <h1 className="text-center fw-semibold">Our Story</h1>
+                <p className='fs-5'>
+                    Baby Bloom is a dedicated provider of high-quality baby care products and daily essentials designed to support the health, comfort, and development of infants and toddlers. Our mission is to offer safe, reliable, and effective solutions for parents seeking the best for their children.
+                </p>
+                <p className='fs-5'>
+                    We prioritize quality, safety, and sustainability in every product we offer. All items in our range undergo strict quality control and adhere to national and international safety standards. From skincare to feeding essentials, hygiene products, and accessories, our catalog is curated to meet the everyday needs of modern parents.
+                </p>
+                <p className='fs-5'>
+                    Our team includes healthcare professionals, product experts, and customer support specialists who work together to ensure that every product and service we deliver meets the expectations of safety and care that families deserve.
+                </p>
+                <p className='fs-5'>
+                    At Baby Bloom, we are committed to building trust through transparency, ethical sourcing, and a focus on continuous improvement to serve the evolving needs of parents and their children.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
